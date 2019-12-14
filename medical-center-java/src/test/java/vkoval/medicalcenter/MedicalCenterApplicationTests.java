ce06c8217c936dc2f75ca7f2b2151bdf6520d3d2
Nikolai023 <nn_gerasimenko@mail.ru>
2019-12-15 00:18:28 +0700

    initial

---
+++ medical-center-java/src/test/java/vkoval/medicalcenter/MedicalCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package vkoval.medicalcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
