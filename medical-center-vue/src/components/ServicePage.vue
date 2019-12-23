399ddbf980df78a92df5216ed12e537d704deab6
vladislav-koval <matsu.touta1981@gmail.com>
2019-12-24 00:23:19 +0700

    crutch in calendar

--- medical-center-vue/src/components/ServicePage.vue
+++ medical-center-vue/src/components/ServicePage.vue
@@ -39,61 +39,7 @@
                   <th>Вс</th>
                 </tr>
                 </thead>
-                <tbody>
-                <tr>
-                  <td class="off"><a href="#">26</a></td>
-                  <td class="off"><a href="#">27</a></td>
-                  <td class="off"><a href="#">28</a></td>
-                  <td class="off"><a href="#">29</a></td>
-                  <td class="off"><a href="#">30</a></td>
-                  <td class="off"><a href="#">31</a></td>
-                  <td><a href="#">1</a></td>
-                </tr>
-                <tr>
-                  <td><a href="#">2</a></td>
-                  <td><a href="#">3</a></td>
-                  <td><a href="#">4</a></td>
-                  <td><a href="#">5</a></td>
-                  <td><a href="#">6</a></td>
-                  <td><a href="#">7</a></td>
-                  <td><a href="#">8</a></td>
-                </tr>
-                <tr>
-                  <td><a href="#">9</a></td>
-                  <td><a href="#">10</a></td>
-                  <td><a href="#">11</a></td>
-                  <td><a href="#">12</a></td>
-                  <td><a href="#">13</a></td>
-                  <td><a href="#">14</a></td>
-                  <td><a href="#">15</a></td>
-                </tr>
-                <tr>
-                  <td><a href="#">16</a></td>
-                  <td><a href="#">17</a></td>
-                  <td><a href="#">18</a></td>
-                  <td><a href="#">19</a></td>
-                  <td><a href="#">20</a></td>
-                  <td><a href="#">21</a></td>
-                  <td><a href="#">22</a></td>
-                </tr>
-                <tr>
-                  <td><a href="#">23</a></td>
-                  <td><a href="#">24</a></td>
-                  <td><a href="#">25</a></td>
-                  <td><a href="#">26</a></td>
-                  <td><a href="#">27</a></td>
-                  <td class="active"><a href="#">28</a></td>
-                  <td><a href="#">29</a></td>
-                </tr>
-                <tr>
-                  <td><a href="#">30</a></td>
-                  <td><a href="#">31</a></td>
-                  <td class="off"><a href="#">1</a></td>
-                  <td class="off"><a href="#">2</a></td>
-                  <td class="off"><a href="#">3</a></td>
-                  <td class="off"><a href="#">4</a></td>
-                  <td class="off"><a href="#">5</a></td>
-                </tr>
+                <tbody v-html="calend">
                 </tbody>
               </table>
               <div class="times">
@@ -119,7 +65,6 @@
                  v-html="editArea.description"
                  @focusout="onDescriptionInput($event)"
             />
-            <div v-html="calend"></div>
           </div>
         </div>
       </div>
@@ -131,32 +76,31 @@
   import ServicesService from '../service/ServicesService';
   import AuthService from '@/service/AuthService';
 
-
   function getDay(date) { // получить номер дня недели, от 0 (пн) до 6 (вс)
     let day = date.getDay();
     if (day === 0) day = 7; // сделать воскресенье (0) последним днем
     return day - 1;
   }
 
-  function createCalendar(elem, year, month) {
+  function createCalendar(year, month) {
     const mon = month - 1; // месяцы в JS идут от 0 до 11, а не от 1 до 12
     const d = new Date(year, mon);
     let temp = new Date(year, mon);
 
-    let table = '<table><tr><th>пн</th><th>вт</th><th>ср</th><th>чт</th><th>пт</th><th>сб</th><th>вс</th></tr><tr>';
+    let table = '<tr>';
     let k = 1;
     temp.setDate(d.getDate() - getDay(d) - 1);
     // eslint-disable-next-line no-plusplus
     for (let i = 0; i < getDay(d); i++) {
       temp.setDate(temp.getDate() + 1);
-      table += `<td>${temp.getDate()}</td>`;
+      table += `<td class="off"><a href="#">${temp.getDate()}</a></td>`;
     }
 
     // <td> ячейки календаря с датами]
     temp = new Date(year, mon);
     temp.setDate(temp.getDate() + 1);
     while (d.getMonth() === mon) {
-      table += `<td>${d.getDate()}</td>`;
+      table += `<td><a href="#">${d.getDate()}</a></td>`;
 
       if (getDay(d) % 7 === 6) { // вс, последний день - перевод строки
         table += '</tr>';
@@ -170,41 +114,35 @@
       d.setDate(d.getDate() + 1);
     }
 
-    let j = 1;
+    let j = 0;
     // eslint-disable-next-line no-use-before-define
     if (getDay(d) !== 0) {
       // eslint-disable-next-line no-plusplus
       for (let i = getDay(d); i < 7; i++) {
         // eslint-disable-next-line no-plusplus
-        table += `<td> ${j++}</td>`;
+        table += `<td class="off"><a href="#" class="off">${j++}</a></td>`;
       }
     }
     // eslint-disable-next-line no-plusplus
     for (let i = k; i < 6; i++) {
       // eslint-disable-next-line no-plusplus
-      table += ('<tr>'
-        // eslint-disable-next-line no-plusplus
-        + `<td> ${j++}</td>`
-        // eslint-disable-next-line no-plusplus
-        + `<td> ${j++}</td>`
-        // eslint-disable-next-line no-plusplus
-        + `<td> ${j++}</td>`
-        // eslint-disable-next-line no-plusplus
-        + `<td> ${j++}</td>`
-        // eslint-disable-next-line no-plusplus
-        + `<td> ${j++}</td>`
-        // eslint-disable-next-line no-plusplus
-        + `<td> ${j++}</td>`
-        // eslint-disable-next-line no-plusplus
-        + `<td> ${j++}</td>`
-        + '</tr>');
+      table += `<tr>
+                      <td class="off"><a href="#"> ${j += 1} </a></td>
+                      <td class="off"><a href="#"> ${j += 1} </a></td>
+                      <td class="off"><a href="#"> ${j += 1} </a></td>
+                      <td class="off"><a href="#"> ${j += 1} </a></td>
+                      <td class="off"><a href="#"> ${j += 1} </a></td>
+                      <td class="off"><a href="#"> ${j += 1} </a></td>
+                      <td class="off"><a href="#"> ${j += 1} </a></td>
+                  </tr>`;
     }
 
     // закрыть таблицу
-    table += '</tr></table>';
+    table += '</tr>';
 
     return table;
   }
+
 
   export default {
     data() {
@@ -212,7 +150,7 @@
         isEditing: false,
         isAdmin: AuthService.isAdmin(),
 
-        calend: createCalendar(2010, 2),
+        calend: createCalendar(2012, 2),
         service: ServicesService.getServiceById(this.$route.params.id),
         editArea: {
           title: '',
