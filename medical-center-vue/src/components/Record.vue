a6a285feb978f7faa5378fde765743ce005e3807
Nikolai023 <nn_gerasimenko@mail.ru>
2019-12-31 12:50:46 +0700

    personal cabinet date format fix

--- medical-center-vue/src/components/Record.vue
+++ medical-center-vue/src/components/Record.vue
@@ -5,11 +5,9 @@
       <h3 class="record-card__title"><a>{{rec.title}}</a></h3>
       <div class="record-card__description">
         Запись
-        <div class="record-card__date">на {{rec.record.getDay() + '.' +
-          (rec.record.getMonth() + 1) + '.' + rec.record.getFullYear()}}
+        <div class="record-card__date">на {{rec.record.toLocaleDateString()}}
         </div>
-        <div class="record-card__time"> в {{rec.record.getHours() + ':' +
-          rec.record.getMinutes()}}
+        <div class="record-card__time"> в {{rec.record.toLocaleTimeString()}}
         </div>
       </div>
     </div>
@@ -19,8 +17,5 @@
 <script>
   export default {
     props: ['rec'],
-    beforeMount() {
-      this.rec.record.format('yyyy-mm-dd');
-    },
   };
 </script>
